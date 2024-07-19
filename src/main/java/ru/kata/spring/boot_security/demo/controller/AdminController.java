45b6c6e8d8b2a0e9f24bdbfa9c0260da02aaa753
AlexanderAlexeevich <167253947+AlexanderAlexeevich@users.noreply.github.com>
2024-07-19 23:05:42 +0300

    begin

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,53 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public AdminController(UserService userService, RoleRepository roleRepository) {
+        this.userService = userService;
+        this.roleRepository = roleRepository;
+    }
+
+    @GetMapping
+    public String showAllUsers(ModelMap model, Principal principal) {
+        model.addAttribute("curUser", userService.findByUsername(principal.getName()));
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("roles", roleRepository.findAll());
+        model.addAttribute("newUser", new User());
+        return "admin";
+    }
+
+    @PostMapping("/add")
+    public String addUser(@ModelAttribute("newUser") User user) {
+        userService.save(user);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.update(user);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/delete")
+    public String deleteUser(@RequestParam(value = "id") Long id) {
+        userService.delete(id);
+        return "redirect:/admin";
+    }
+}
